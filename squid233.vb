16b7b1b6a8e858255e4f9f0fd9a950a94fbc1670
Squid233 <60126026+squid233@users.noreply.github.com>
2020-07-12 21:00:03 +0800

    Create squid233.vb

---
+++ squid233.vb
@@ -0,0 +1,14 @@
+Module Squid233
+
+    Sub Main()
+
+        Dim squid233 as String
+        squid233 = "squid233"
+
+        While True
+            Console.WriteLine(squid233)
+        End While
+
+    End Sub
+
+End Module
