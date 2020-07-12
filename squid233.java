ebed00b00f90de106ca5d4152ba81f3e55c64e5f
Squid233 <60126026+squid233@users.noreply.github.com>
2020-07-12 20:37:27 +0800

    Create squid233.java

---
+++ squid233.java
@@ -0,0 +1,17 @@
+public class squid233 {
+
+    public static final String SQUID233 = "squid233";
+
+    public static void main(String[] Squid233) {
+        squid233(SQUID233);
+    }
+
+    public static void squid233(String squid233) {
+        if (squid233.equals(squid233)) {
+            for (;;) {
+                System.out.println(squid233);
+            }
+        }
+    }
+
+}
