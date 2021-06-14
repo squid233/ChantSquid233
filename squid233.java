90fe96ecc417d43b9c6feeeab4d0f6b288526ddb
squid233 <60126026+squid233@users.noreply.github.com>
2021-06-14 16:27:25 +0800

    Update squid233.java

--- squid233.java
+++ squid233.java
@@ -1,17 +1,5 @@
-public class squid233 {
-
-    public static final String SQUID233 = "squid233";
-
-    public static void main(String[] Squid233) {
-        squid233(SQUID233);
+class squid233{
+    public static void main(String[]squid233) {
+        while(true){System.out.println("squid233");}
     }
-
-    public static void squid233(String squid233) {
-        if (squid233.equals(squid233)) {
-            for (;;) {
-                System.out.println(squid233);
-            }
-        }
-    }
-
 }
