332579704ab1153c55cc247f4029a2db81b60615
squid233 <60126026+squid233@users.noreply.github.com>
2021-06-14 16:29:11 +0800

    Update squid233.vb

--- squid233.vb
+++ squid233.vb
@@ -1,14 +1,7 @@
 Module Squid233
-
     Sub Main()
-
-        Dim squid233 as String
-        squid233 = "squid233"
-
         While True
-            Console.WriteLine(squid233)
+            Console.WriteLine("squid233")
         End While
-
     End Sub
-
 End Module
